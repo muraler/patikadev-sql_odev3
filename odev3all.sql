de37bc77cccb3075710b6abb8dfa697c582ccca7
muraler <109878632+muraler@users.noreply.github.com>
2022-08-08 21:14:22 +0300

    Create odev3all.sql

---
+++ odev3all.sql
@@ -0,0 +1,17 @@
+select * from country
+where country like 'A%a';
+
+
+
+select * from country
+where country like '______a';
+
+
+
+SELECT title FROM film
+WHERE title ILIKE '%t%t%t%t%'
+
+
+
+select * from film
+where title like 'C%' and length > 90 and rental_rate = 2.99
